c39ff710ce16fef3965a38856aa25652123e8ace
wuhan <912056724@qq.com>
2023-01-31 18:50:17 +0800

    f2-vue3-demo

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <canvas id="container" class="container"></canvas>
+</template>
+<script setup lang="jsx">
+import { onMounted } from 'vue';
+import { Canvas, Chart, Axis, Tooltip, ScrollBar, Interval } from '@antv/f2';
+
+onMounted(() => {
+  init();
+});
+
+function formatNumber(n) {
+  return String(Math.floor(n * 100) / 100).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const init = () => {
+  const context = document.getElementById('container').getContext('2d');
+  fetch('https://gw.alipayobjects.com/os/antfincdn/ZpWsTPpY6%26/steps.json')
+    .then((res) => res.json())
+    .then((data) => {
+      const { props } = (
+        <Canvas context={context} pixelRatio={window.devicePixelRatio}>
+          <Chart data={data}>
+            <Axis field="date" type="timeCat" tickCount={5} />
+            <Axis field="steps" formatter={formatNumber} />
+            <Interval x="date" y="steps" />
+            <ScrollBar mode="x" range={[0.1, 0.3]} />
+            <Tooltip />
+          </Chart>
+        </Canvas>
+      );
+      console.time('render');
+      const chart = new Canvas(props);
+      chart.render();
+      console.timeEnd('render');
+    });
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 300px;
+}
+</style>
